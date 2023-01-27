089949667533cb7ab17dca0411e4e729c0fe6209
Leandro Assis <leandrou9@gmail.com>
2023-01-27 11:00:09 -0300

    Commit inicial

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://celaskane:6rDmIe1qqjrSCCA9@ac-vx8ihxu-shard-00-00.x5bxntx.mongodb.net:27017,ac-vx8ihxu-shard-00-01.x5bxntx.mongodb.net:27017,ac-vx8ihxu-shard-00-02.x5bxntx.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-133p5l-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
 };
